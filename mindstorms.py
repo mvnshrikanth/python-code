f268c793b36c20fae05905c73465aca2d1c18bc4
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-06-24 17:26:16 -0400

    Added circle draw

--- mindstorms.py
+++ mindstorms.py
@@ -1,28 +1,26 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window=turtle.Screen()
     window.bgcolor("red")
 
-    brad = turtle.Turtle()
-    brad.shape("turtle")
+    brad=turtle.Turtle()
     brad.color("yellow")
+    brad.shape("turtle")
     brad.speed(2)
-
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    draw_square(brad)
 
     angie=turtle.Turtle()
+    angie.color("blue")
     angie.shape("arrow")
-    angie.color("yellow")
+    angie.speed(2)
     angie.circle(100)
 
     window.exitonclick()
 
-draw_square()
+draw_art()
