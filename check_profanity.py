5d015a8a3364c184bc9da71d5f387fbe9f34287c
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-06-24 20:39:35 -0400

    create a class and use it

--- check_profanity.py
+++ check_profanity.py
@@ -1,6 +1,21 @@
+from urllib.request import urlopen, pathname2url
+
 def read_text():
     quotes = open("E:\Work\Python\python-code\movie_quotes.txt")
-    print(quotes.read())
+    file_contenets = quotes.read()
     quotes.close()
+    check_profanity(file_contenets)
+
+def check_profanity(text_to_check):
+    connection = urlopen(u"http://www.wdylike.appspot.com/?q="+pathname2url(text_to_check))
+    output = connection.read()
+    # print(output)
+    if output == b'true':
+        print("Profanity spotted!")
+    elif output == b'false':
+        print("Looks good.")
+    else:
+        print("Document could not be scanned.")
+    connection.close()
 
 read_text()
---
+++ entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of boy and his toys that come to life",
+                        "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                        "A marine on an alien planet",
+                        "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg",
+                        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+avatar.show_trailer()
